978056033f271052987b8e6f056d751789f41304
Timothy <57668523+timothyckw@users.noreply.github.com>
2020-09-15 20:58:58 +0800

    remove redundant code add multi sign

--- bitcoin/src/main/java/org/ckw/task/AddressGenerator.java
+++ bitcoin/src/main/java/org/ckw/task/AddressGenerator.java
@@ -4,16 +4,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.bitcoinj.core.*;
 import org.bitcoinj.crypto.*;
 import org.bitcoinj.params.TestNet3Params;
+import org.bitcoinj.script.Script;
+import org.bitcoinj.script.ScriptBuilder;
 import org.bitcoinj.wallet.DeterministicKeyChain;
 import org.bitcoinj.wallet.DeterministicSeed;
-import org.ckw.bean.Account;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import org.bitcoinj.params.MainNetParams;
 import sun.security.provider.SecureRandom;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.ckw.bean.Account;
+import org.ckw.bean.MultiSignAccount;
+
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 
@@ -23,30 +28,24 @@ import static org.bitcoinj.crypto.HDUtils.*;
 @RestController
 public class AddressGenerator {
 
-  private static final MainNetParams MAINNET = MainNetParams.get();
-  private static final TestNet3Params TESTNET = TestNet3Params.get();
+  private static final MainNetParams MAIN_NET = MainNetParams.get();
+  private static final TestNet3Params TEST_NET = TestNet3Params.get();
 
   @RequestMapping(value = "/getTimeStamp")
   public long getTimeStamp() {
     return Calendar.getInstance().getTimeInMillis();
   }
 
-  @RequestMapping(value = "/getSegwitAddress")
-  public String getBench12Address() throws MnemonicException.MnemonicLengthException {
-    NetworkParameters params = MainNetParams.get();
+  @RequestMapping(value = "/getECKey")
+  public String getECKey() {
     ECKey ecKey = new ECKey();
-
-    System.out.format("Private Key => %s\n", ecKey.getPrivateKeyAsHex());
-    System.out.format("Public Key => %s\n", ecKey.getPublicKeyAsHex());
-
-    SegwitAddress segwitAddress = SegwitAddress.fromKey(MAINNET, ecKey);
-    System.out.println("P2WPKH address ：" + segwitAddress.toBech32());
+    log.info(ecKey.toString());
 
     return ecKey.toString();
   }
 
-  @RequestMapping(value = "/getP2WPKHAddress")
-  public Account getP2WPKHAddress() throws MnemonicException.MnemonicLengthException {
+  @RequestMapping(value = "/getAccount")
+  public synchronized Account getAccount() throws MnemonicException.MnemonicLengthException {
     Account account = new Account();
 
     SecureRandom secureRandom = new SecureRandom();
@@ -54,33 +53,60 @@ public class AddressGenerator {
     secureRandom.engineNextBytes(entropy);
     List<String> mnemonic = MnemonicCode.INSTANCE.toMnemonic(entropy);
 
-    String path = "M/84/0/0/0";
+    String path = "M/84/0/0/0/0";
     DeterministicSeed deterministicSeed = new DeterministicSeed(mnemonic, null, "", 0L);
     DeterministicKeyChain deterministicKeyChain = DeterministicKeyChain.builder().seed(deterministicSeed).build();
-    BigInteger privateKey = deterministicKeyChain
-            .getKeyByPath(parsePath(path), true).getPrivKey();
-
+    BigInteger privateKey = deterministicKeyChain.getKeyByPath(parsePath(path), true).getPrivKey();
     ECKey ecKey = ECKey.fromPrivate(privateKey);
-    SegwitAddress segwitAddress = SegwitAddress.fromKey(MAINNET, ecKey);
-    LegacyAddress legacyAddress = LegacyAddress.fromKey(MAINNET, ecKey);
+    SegwitAddress segwitAddress = SegwitAddress.fromKey(MAIN_NET, ecKey);
+    LegacyAddress legacyAddress = LegacyAddress.fromKey(MAIN_NET, ecKey);
 
+    // update account
     account.setPublicKey(ecKey.getPublicKeyAsHex());
     account.setPrivateKey(ecKey.getPrivateKeyAsHex());
-    account.setSegwitAddress(segwitAddress.toBech32());
+    account.setSegWitAddress(segwitAddress.toBech32());
     account.setLegacyAddress(legacyAddress.toBase58());
     account.setMnemonicCode(mnemonic);
-
-    // debug
-    System.out.println("--");
-    for (int i = 0; i < mnemonic.size(); ++i) {
-      System.out.print(mnemonic.get(i) + " ");
-    }
-    System.out.println("\n--");
-    System.out.println(ecKey.getPrivateKeyAsHex());
-    System.out.println(ecKey.getPublicKeyAsHex());
-    System.out.println("P2WPKH address ：" + segwitAddress.toBech32());
-    System.out.println("Legacy address ：" + legacyAddress.toBase58());
+    log.info(account.toString());
 
     return account;
+  }
+
+  @RequestMapping(value = "/getMultiSignAccount")
+  public synchronized MultiSignAccount getMultiSignAccount() {
+    MultiSignAccount multiSignAccount = new MultiSignAccount();
+
+    // generate random ECKey
+    ECKey ecKey1 = new ECKey();
+    ECKey ecKey2 = new ECKey();
+    ECKey ecKey3 = new ECKey();
+
+    // create a 2-of-3 multi sign redeemScript (output script)
+    List<ECKey> keys = Arrays.asList(ecKey1, ecKey2, ecKey3);
+
+    // create scrip for generate legacy address
+    Script redeemScript = ScriptBuilder.createRedeemScript(2, keys);
+    Script script = ScriptBuilder.createP2SHOutputScript(redeemScript);
+    LegacyAddress legacyAddress = LegacyAddress.fromScriptHash(MAIN_NET, script.getPubKeyHash());
+
+    // update multi sign account
+    keys.forEach((i) -> multiSignAccount.addPublicKey(i.getPublicKeyAsHex()));
+    multiSignAccount.setMultiSigAddress(legacyAddress.toBase58());
+    log.info(multiSignAccount.toString());
+
+    return multiSignAccount;
+  }
+
+  public static boolean isBTCValidAddress(String address) {
+    try {
+      LegacyAddress legacyAddress = LegacyAddress.fromBase58(MAIN_NET, address);
+      if (legacyAddress != null) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (Exception e) {
+      return false;
+    }
   }
 }
